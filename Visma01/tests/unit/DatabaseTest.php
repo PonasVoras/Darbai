af42c2de1b4a71bb487ad5e2562cc3e478aba061
PonasVoras8 <debesysmelyni@gmail.com>
2019-07-26 09:24:17 +0300

    Cleaning

---
+++ Visma01/tests/unit/DatabaseTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+
+//TODO Test: Database, Query Builder, Hyphenate, API
+
+class DatabaseTest extends TestCase
+{
+    public function testFirstTime(){
+        $this->assertTrue(true);
+    }
+}
